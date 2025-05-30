af54b9b32dd28e768bdbf1626e82aada592ea909
Ed Sweeney <ed@onextent.com>
2025-05-29 21:02:20 -0700

    refactor

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(clippy::cast_precision_loss)] // We accept precision loss for f32 conversions
+#![allow(clippy::needless_pass_by_value)] // Bevy systems require owned Res parameters
+#![allow(clippy::multiple_crate_versions)] // Bevy dependencies have multiple versions
+
+pub mod camera;
+pub mod parser;
+pub mod types;
+pub mod ui;
+pub mod visualization;
+
+pub use types::{GraphData, NodeType};
